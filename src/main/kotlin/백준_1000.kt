4a47ffe6ed42cd09f75680689c47e672b0781edd
unanchoi <unanc2@gmail.com>
2024-01-02 21:15:48 +0900

    initial commit

---
+++ src/main/kotlin/백준_1000.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class 백준_1000 {
+
+}
+
+fun main(args: Array<String>) {
+    val (a,b) = readln().split(" ").map { it. toInt()}
+    println(a+b)
+}
---
+++ src/main/kotlin/백준_1009.kt
@@ -0,0 +1,20 @@
+package org.example
+
+
+fun main(args: Array<String>) {
+    val n = readln().toInt()
+
+    for (i in 1..n) {
+        var (a, b) = readln().split(" ").map { it.toInt() }
+        var result = a
+        for (j in 1..< b)  {
+            result = (result * a) % 10
+        }
+
+        if (result % 10 == 0) {
+            println(10)
+        } else {
+            println(result % 10)
+        }
+    }
+}
